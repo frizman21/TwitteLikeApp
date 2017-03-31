cedf2b8c15f6ca9a638d19ae84d558dd83eb001e
originate <mike@aesel.systems>
2017-03-30 22:38:49 -0500

    thru 8.2.3 - off to bed

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  
+  def new
+  end
+
+  def create
+    
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      
+      log_in user
+      redirect_to user # shortcut
+      #redirect_to user_url(user) # explicit version
+
+    else
+      flash.now[:danger] = 'Invalid email/password combination' 
+      render 'new'
+    end
+
+  end
+
+  def destroy
+  end
+
+end
